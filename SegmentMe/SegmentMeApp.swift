f938262bc770ff41c76ec66f329d09aae736f986
Benjamin Axline <baxline@bu.edu>
2024-06-20 14:10:03 -0400

    Initial Commit

---
+++ SegmentMe/SegmentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentMeApp.swift
+//  SegmentMe
+//
+//  Created by Benjamin Axline on 6/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
