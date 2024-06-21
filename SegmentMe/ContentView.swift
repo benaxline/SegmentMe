cf4275cf9011a31ab2d9039f7c1344b9a8ccf9bd
Benjamin Axline <baxline@bu.edu>
2024-06-21 14:24:55 -0400

    Added multiple image upload and displaying all uploaded photos.

--- SegmentMe/ContentView.swift
+++ SegmentMe/ContentView.swift
@@ -6,18 +6,47 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { i in
+                    selectedImages[i]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+                
+            
+            PhotosPicker("Select Images",
+                         selection: $pickerItems,
+                         matching: .images)
+            
+            Spacer()
         }
-        .padding()
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
+        .frame(minWidth: 500, minHeight: 500)
     }
 }
+
+
 
 #Preview {
     ContentView()
