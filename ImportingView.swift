b24e79254ff2fb873b61c209096af386c144623f
Benjamin Axline <baxline@bu.edu>
2024-06-21 17:10:12 -0400

    Added image names beside images

--- ImportingView.swift
+++ ImportingView.swift
@@ -10,20 +10,63 @@ import SwiftUI
 struct ImportingView: View {
     @State private var importing = false
     @State private var images = [NSImage]()
+    @State private var imageNames = [String]()
     @State private var selectedImage: NSImage?
     
     var body: some View {
         NavigationSplitView {
             List(images.indices, id: \.self) { index in
-                Image(nsImage: images[index])
-                    .resizable()
-                    .scaledToFit()
-                    .frame(height: 100)
-                    .onTapGesture {
-                        selectedImage = images[index]
+                HStack {
+                    Image(nsImage: images[index])
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 100)
+                        .onTapGesture {
+                            selectedImage = images[index]
                     }
+                    Spacer()
+                    
+                    Text("\(imageNames[index])")
+                }
             }
             .navigationTitle("Your Photos")
+            .frame(minWidth: 300)
+            .toolbar {
+                ToolbarItem {
+                    Button(action: {
+                        importing = true
+                    }){
+                        Label("", systemImage: "square.and.arrow.down")
+                    }
+                    .padding()
+                    .fileImporter(
+                        isPresented: $importing,
+                        allowedContentTypes: [.image],
+                        allowsMultipleSelection: true) {
+                            result in
+                            switch result {
+                            case .success(let files):
+                                files.forEach { file in
+                                    // gain access
+                                    let access = file.startAccessingSecurityScopedResource()
+                                    
+                                    // if we don't have access
+                                    if !access {
+                                        return
+                                    }
+                                    
+                                    // put into nav split
+                                    putIntoNavSplit(from: file)
+                                    
+                                    // release access
+                                    file.stopAccessingSecurityScopedResource()
+                                }
+                            case .failure(let error):
+                                print(error)
+                            }
+                        }
+                }
+            }
         } detail: {
             if let selectedImage = selectedImage {
                 Image(nsImage: selectedImage)
@@ -31,52 +74,23 @@ struct ImportingView: View {
                     .scaledToFit()
                     .navigationTitle("Photo Detail")
             } else {
+                Spacer()
                 Text("Select a Photo")
                     .font(.largeTitle)
                     .foregroundStyle(.gray)
+                Spacer()
             }
         }
         
-        Button("Import Photos", systemImage: "square.and.arrow.up") {
-            importing = true
-        }
-        .fileImporter(
-            isPresented: $importing,
-            allowedContentTypes: [.image],
-            allowsMultipleSelection: true) {
-                result in
-                switch result {
-                case .success(let files):
-                    files.forEach { file in
-                        // gain access
-                        let access = file.startAccessingSecurityScopedResource()
-                        
-                        // if we don't have access
-                        if !access {
-                            return
-                        }
-                        
-                        // put into nav split
-                        putIntoNavSplit(from: file)
-                        
-                        // release access
-                        file.stopAccessingSecurityScopedResource()
-                    }
-                case .failure(let error):
-                    print(error)
-                }
-            }
-//            .onChange(of: <#T##Equatable#>, <#T##action: (Equatable, Equatable) -> Void##(Equatable, Equatable) -> Void##(_ oldValue: Equatable, _ newValue: Equatable) -> Void#>)
-            .buttonStyle(.bordered)
-        
-            
     }
-//        .frame(minWidth: 500, minHeight: 500)
+
+
     func putIntoNavSplit(from file: URL) {
         do {
             let data = try Data(contentsOf: file)
             if let nsImage = NSImage(data: data) {
                 images.append(nsImage)
+                imageNames.append(file.lastPathComponent)
             }
         } catch {
             print("Error loading image: \(error)")
--- SegmentMe/ContentView.swift
+++ SegmentMe/ContentView.swift
@@ -13,45 +13,10 @@ struct ContentView: View {
     @State private var selectedImages = [Image]()
     
     var body: some View {
-        
-        
-        
-        
+
         VStack {
-            
-            Text("Welcome to SegmentMe!")
-                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                .padding()
+        
             ImportingView()
-            
-            
-            
-//            ScrollView {
-//                ForEach(0..<selectedImages.count, id: \.self) { i in
-//                    selectedImages[i]
-//                        .resizable()
-//                        .scaledToFit()
-//                }
-//            }
-//                
-//            Spacer()
-//            
-//            PhotosPicker("Select Images",
-//                         selection: $pickerItems,
-//                         matching: .images)
-//            
-//            Spacer()
-//        }
-//        .onChange(of: pickerItems) {
-//            Task {
-//                selectedImages.removeAll()
-//                
-//                for item in pickerItems {
-//                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-//                        selectedImages.append(loadedImage)
-//                    }
-//                }
-//            }
         }
         .frame(minWidth: 500, minHeight: 500)
     }
